01b3305b67d0378c38f988c31bf17d53745614c1
Peter Thor <83021617+peterthor1@users.noreply.github.com>
2025-06-24 19:18:23 -0400

    Add Insertion & Deletion Integration Tests

---
+++ tests/integration_tests.rs
@@ -0,0 +1,13 @@
+use b_tree::BPlusTree;
+
+#[test]
+fn insertion_and_deletion() {
+    let mut tree = BPlusTree::new(3);
+
+    for i in 0..=100 {
+        let _ = tree.insert_one(i, i);
+    }
+    for i in 0..=100 {
+        assert_eq!(tree.search(i), Some(i));
+    }
+}
